41b0398a8f3d05b65f85b85909ec70a5454549bc
VirLg <19virlg@gmail.com>
2024-06-30 06:59:01 +0300

    checked in local type

--- src/app/components/utils/data/dataSidebar.js
+++ src/app/components/utils/data/dataSidebar.js
@@ -1,16 +1,28 @@
 export const LokationData = [
-  { label: 'Remote', checked: false, idxEl: 0 },
-  { label: 'Onsite', checked: false, idxEl: 1 },
-  { label: 'Hybrid', checked: false, idxEl: 2 },
+  { label: 'Remote', checked: true, idxEl: 0 },
+  { label: 'Onsite', checked: true, idxEl: 1 },
+  { label: 'Hybrid', checked: true, idxEl: 2 },
 ];
 export const FunctionData = [
   { label: 'Accounting & Finance', checked: false, idxEl: 0 },
   { label: 'Business Support', checked: false, idxEl: 1 },
-  { label: 'FunData Science & Analyticsction', checked: false, idxEl: 2 },
+  {
+    label: 'FunData Science & Analyticsction',
+    checked: false,
+    idxEl: 2,
+  },
   { label: 'Design, Copy & Production', checked: false, idxEl: 3 },
   { label: 'Education', checked: false, idxEl: 4 },
-  { label: 'Financial Services - Front Office', checked: false, idxEl: 5 },
-  { label: 'Financial Services - Operations', checked: false, idxEl: 6 },
+  {
+    label: 'Financial Services - Front Office',
+    checked: false,
+    idxEl: 5,
+  },
+  {
+    label: 'Financial Services - Operations',
+    checked: false,
+    idxEl: 6,
+  },
   { label: 'General Management', checked: false, idxEl: 7 },
   { label: 'Healthcare & Lifesciences', checked: false, idxEl: 8 },
   { label: 'Human Resources', checked: false, idxEl: 9 },
@@ -19,7 +31,11 @@ export const FunctionData = [
   { label: 'Marketing', checked: false, idxEl: 12 },
 ];
 export const IndustryData = [
-  { label: 'Aviation, Transport & Logistics', checked: false, idxEl: 0 },
+  {
+    label: 'Aviation, Transport & Logistics',
+    checked: false,
+    idxEl: 0,
+  },
   { label: 'Consumer', checked: false, idxEl: 1 },
   { label: 'Financial Services', checked: false, idxEl: 2 },
   { label: 'Healthcare & Lifesciences', checked: false, idxEl: 3 },
